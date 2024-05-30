367c6351bd8571124dbb1c79bf23620c19b35ab7
Delthar <nick@matys.net>
2024-05-30 17:55:09 +0200

    Implemented basic list operations for natively generated events.

---
+++ BookingVerseFinal/Components/Models/Event.cs
@@ -0,0 +1,24 @@
+﻿namespace BookingVerse.Components.Models
+{
+    public class Event
+    {
+        public string EventID { get; set; }
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+        public string Location { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+        public string? Description { get; set; }
+
+        public Event(string EventID, string Title, DateTime Date, string Location, string Category, decimal Price, string Description)
+        {
+            this.EventID = EventID;
+            this.Title = Title;
+            this.Date = Date;
+            this.Location = Location;
+            this.Category = Category;
+            this.Price = Price;
+            this.Description = Description;
+        }
+    }
+}
