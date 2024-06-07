2ec21500bebd1a7477185e53f38068c2a32bd1d0
Sergy2000 <sergen2000@gmail.com>
2024-06-07 14:12:23 +0200

    additional resource

--- BookingVerseFinalAPI/Database/ApplicationDbContext.cs
+++ BookingVerseFinalAPI/Database/ApplicationDbContext.cs
@@ -13,3 +13,19 @@ namespace BookingVerseFinalAPI.Database
         public DbSet<Order> Orders { get; set; }
     }
 }
+
+/*
+ * 
+ * 
+ *
+ *
+ *
+ *dotnet add package Microsoft.AspNetCore.Components.WebView.Maui
+dotnet add package Microsoft.Extensions.Http
+Microsoft.AspNetCore.Authentication.JwtBearer: To handle JWT authentication.
+Microsoft.IdentityModel.Tokens: To manage JWT token validation.
+Microsoft.EntityFrameworkCore: Core package for Entity Framework Core.
+Microsoft.EntityFrameworkCore.SqlServer: Provider for SQL Server.
+Microsoft.EntityFrameworkCore.Design: Tools for Entity Framework Core.
+Microsoft.EntityFrameworkCore.Tools: Tools for managing migrations and scaffolding.
+ **/
