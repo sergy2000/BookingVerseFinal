24b0ba46b8197348bcdbed2739d3eb27cef28322
Sergy2000 <sergen2000@gmail.com>
2024-06-07 14:06:24 +0200

    Added basic structure to work on backend

--- BookingVerseFinalAPI/Database/ApplicationDbContext.cs
+++ BookingVerseFinalAPI/Database/ApplicationDbContext.cs
@@ -1,6 +1,15 @@
-﻿namespace BookingVerseFinalAPI.Database
+﻿using BookingVerseFinalAPI.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookingVerseFinalAPI.Database
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Account> Users { get; set; }
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Order> Orders { get; set; }
     }
 }
